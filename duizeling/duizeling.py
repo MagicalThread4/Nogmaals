8dda17ecc57d061929c35ca2486bec0208672d42
Rob Koster <robkoster2907@gmail.com>
2021-09-14 10:28:17 +0200

    Ik heb de eerst verie van de opracht af

--- duizeling/duizeling.py
+++ duizeling/duizeling.py
@@ -1,18 +1,29 @@
 print("Hele uren van de dag")
-for i in range(0,26):
-    if i % 2 == 0:
-        if i in range(0,14):
-            print(str(i) + " AM")
-        if i in range(12,26):
-            print(str(i) + " PM")
+v = 0
+while v < 1:
+    for i in range(0,26):
+        if i % 2 == 0:
+            if i in range(0,14):
+                print(str(i) + " AM")
+            if i in range(12,26):
+                print(str(i) + " PM")
+                v += 1
+
 print("terug tellen van 20 tot 50 en allen even nummer printen")
-for x in range(20,51):
-    if x % 2 == 0:
-        print(x)
+n =  0
+while n < 1:
+    for x in range(20,51):
+        if x % 2 == 0:
+            print(x)
+            n += 1
+
 print("terug tellen van 30")
-for y in range(30, -1, -1):
-    print(y)
-       
+b = 0
+while b < 1:
+    for y in range(30, -1, -1):
+        print(y)
+        b += 1
+  
 print("Tel de cijfers vanaf 50 en doe ze plus elkaar")
 count = 50
 while count <= 2000:
@@ -21,7 +32,6 @@ while count <= 2000:
 
 print("Herhaal een input tot het resultaat daarvan gelijk is aan quit ")
 required_word = 'quit'
-
 while True:
     number = input("Enter the word\n")
     if number == required_word:
@@ -30,28 +40,49 @@ while True:
     else:
         print ("Wrong word try again")
 
-
-day = input("kies een dag: ")
-d = 1
-while d < 2:
-    print("Ma")
-    print("Di")
-    print("Wo")
-    print("Do")
-    print("Vr")
-    print("Sa")
-    print("Zo")
-if input("Ma"):
-    d = d + 1
-if input("Di"):
-    d = d + 1
-if input("Wo"):
-    d = d + 1
-if input("Do"):
-    d = d + 1
-if input("Vr"):
-    d = d + 1
-if input("Sa"):
-    d = d + 1
-if input("Zo"):
-    d = d + 1 
+print("Vraag een dag van de week op ")
+dagen = input("kies een dag: ")
+z = 0
+while z < 1:
+    if dagen == "Ma":
+        print("Ma ")
+        z += 1
+    if dagen == "Di":
+        print("Ma ")
+        print("Di ")
+        z += 1
+    if dagen == "Wo":
+        print("Ma ")
+        print("Di ")
+        print("Wo ")
+        z += 1
+    if dagen == "Do":
+        print("Ma ")
+        print("Di ")
+        print("Wo ")
+        print("Do ")
+        z += 1
+    if dagen == "Vr":
+        print("Ma ")
+        print("Di ")
+        print("Wo ")
+        print("Do ")
+        print("Vr ")
+        z += 1
+    if dagen == "Za":
+        print("Ma ")
+        print("Di ")
+        print("Wo ")
+        print("Do ")
+        print("Vr ")
+        print("Za ")
+        z += 1
+    if dagen == "Zo":
+        print("Zo ")
+        print("Ma ")
+        print("Di ")
+        print("Wo ")
+        print("Do ")
+        print("Vr ")
+        print("Za ")
+        z += 1
